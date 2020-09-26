ded7f3f439aa2a9c8660b3b9cc7675a263d18d51
AlexMa <alex.ma@vanderbilt.edu>
2020-09-25 20:12:44 -0400

    MobileNet
    Switch focus to mobilenet, try to fix tf dataset stuff

--- TestArchM.py
+++ TestArchM.py
@@ -1,5 +1,3 @@
-from MnasNet import MnasNet
-
 import os
 import sys
 from time import time
@@ -7,11 +5,13 @@ import numpy as np
 import tensorflow as tf
 import pickle
 
-from tensorflow import data
 from tensorflow.keras.callbacks import LearningRateScheduler, CSVLogger
-import tensorflow.keras.backend as K
 from functools import partial
 from tensorflow.keras.preprocessing.image import DirectoryIterator, ImageDataGenerator
+
+from tensorflow.keras.applications import MobileNetV2
+import tensorflow.keras.backend as K
+
 
 np.random.seed(1000)
 
@@ -120,48 +120,21 @@ def process_val_image(path):
 #    .map(process_val_image, num_parallel_calls=tf.data.experimental.AUTOTUNE)\
 #    .batch(256)
 
-def preprocess_input(x, data_format=None):
-    if data_format is None:
-        data_format = K.image_data_format()
-    assert data_format in {'channels_last', 'channels_first'}
-
-    if data_format == 'channels_first':
-        # 'RGB'->'BGR'
-        x = x[:, ::-1, :, :]
-        # Zero-center by mean pixel
-
-        x[:, 0, :, :] -= 103.939
-        x[:, 1, :, :] -= 116.779
-        x[:, 2, :, :] -= 123.68
-    else:
-        # 'RGB'->'BGR'
-        x = x[:, :, :, ::-1]
-        # Zero-center by mean pixel
-        x[:, :, :, 0] -= 103.939
-        x[:, :, :, 1] -= 116.779
-        x[:, :, :, 2] -= 123.68
+def preprocess_input(x):
+    x /= 255.
+    x -= 0.5
+    x *= 2.
     return x
 
-dataGen = ImageDataGenerator(preprocessing_function=preprocess_input())
+
+dataGen = ImageDataGenerator(preprocessing_function=preprocess_input)
 
 trainData = DirectoryIterator(trainDirectory,dataGen,target_size=(224, 224))
-
 
 valData = DirectoryIterator(valDirectory, dataGen, target_size=(224, 224))
 
 # Create a MirroredStrategy.
 strategy = tf.distribute.MirroredStrategy()
-
-
-# Learning rate scheduler
-#def scheduler(epoch):
-#    if epoch < 5:
-#        return 0.001
-#    else:
-#        return 0.00001
-
-
-# lrcallback = LearningRateScheduler(scheduler)
 
 # Callback to save model at beginning of every epoch in case of failure
 model_path = "model_data" + "/" + jobid + "/"
@@ -183,18 +156,31 @@ class SaveModelStateCallback(tf.keras.callbacks.Callback):
         self.epoch_time_end = time()
         self.times.append(time() - self.epoch_time_start)
 
+# Learning rate scheduler
+def scheduler(epoch, lr):
+    decay_rate = 0.9
+    decay_step = 10
+    if epoch % decay_step == 0 and epoch:
+        return lr * decay_rate
+    return lr
+
+lrcallback = LearningRateScheduler(scheduler)
+
 # Saving history in case of failure
 csv_logger = CSVLogger(model_path + jobid + "_model_history_log.csv", append=True)
 
 model_state = SaveModelStateCallback()
 
-# Start from checkpoint weights, if it exists
-# old_jobid = 12123214
-# old_chkpt_num = 4
-# model = models.load_model("model_data" + "/" + old_jobid + "/checkpoint{}.h5".format(old_chkpt_num))
+# Callbacks
+callbacks_list = [lrcallback, csv_logger, model_state]
+
+
 
 # SGD optimizer
-sgd = tf.keras.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=False)
+# sgd = tf.keras.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=False)
+
+# Adam optimizer
+adam = tf.keras.optimizers.Adam(lr = 0.01)
 
 # Get top 5 accuracy data instead of just accuracy
 top5_acc = partial(tf.keras.metrics.top_k_categorical_accuracy, k=5)
@@ -202,13 +188,13 @@ top5_acc.__name__ = 'top5_acc'
 
 
 with strategy.scope():
-    model = MnasNet(input_shape=(224,224,3))
-    model.compile(loss=tf.keras.losses.categorical_crossentropy, optimizer=sgd, metrics=[top5_acc])
+    model = MobileNetV2(input_shape=(224,224,3), weights=None)
+    model.compile(loss=tf.keras.losses.categorical_crossentropy, optimizer='adam', metrics=[top5_acc])
 
 start = time()
 
-history = model.fit(trainData, epochs=10, batch_size=64, verbose=2,
-                    validation_data=valData, callbacks=[model_state,csv_logger])
+history = model.fit(trainData, epochs=100, batch_size=96, verbose=2,
+                    validation_data=valData, callbacks=callbacks_list)
 end = time()
 
 epoch_times = model_state.times
@@ -219,4 +205,4 @@ with open(model_path + jobid + 'trainHistoryDict', 'wb') as file_pi:
     pickle.dump(history.history, file_pi)
 
 print("Training time is: " + str(end - start))
-score = model.evaluate(valData, batch_size=128)
+score = model.evaluate(valData, batch_size=96)
